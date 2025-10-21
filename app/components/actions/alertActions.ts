9faf732756dc90389d51a7cd43f17eee05009a12
Marcio Henrique Arrais <marcio.santos@capitaltrade.srv.br>
2025-10-21 11:43:24 -0300

    feat: create method put and delete

--- app/components/actions/alertActions.ts
+++ app/components/actions/alertActions.ts
@@ -4,7 +4,12 @@ import { AlertItems } from "@/lib/generated/prisma"
 import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
 
+export async function getAlerts():Promise<AlertItems[]> {
+    return await prisma.alertItems.findMany();
+}
+
 export async function createAlert(data: FormData): Promise<void> {
+  
   const name = data.get("name") as string
   const responsavel = data.get("responsavel") as string
   const dataAprovacao = data.get("dataAprovacao") as string
@@ -41,6 +46,54 @@ export async function createAlert(data: FormData): Promise<void> {
   revalidatePath('/seed.ts')
 }
 
-export async function getAlerts():Promise<AlertItems[]> {
-    return await prisma.alertItems.findMany();
+export async function updateAlert(data:FormData): Promise<void> {
+  try {
+    const id = data.get('id') as any
+    const name = data.get('name') as string
+    const responsavel = data.get("responsavel") as string
+    const dataAprovacao = data.get("dataAprovacao") as string
+    const tipoCobranca = data.get("tipoCobranca") as string
+    const ptax = data.get("ptax") as string
+    const orcado = data.get("orcado") as string
+    const realizado = data.get("realizado") as string
+    const horaOrcadas = data.get("horaOrcadas") as string
+    const valorHora = data.get("valorHora") as string
+    const obs = data.get("obs") as string
+    const status = data.get("status") as string
+    const prioridade = data.get("prioridade") as string
+    const variacao = data.get("variacao") as string
+
+    await prisma.alertItems.update({
+      where: { id: 1 },
+      data: { 
+        name,
+        responsavel,
+        dataAprovacao,
+        tipoCobranca,
+        ptax,
+        orcado,
+        realizado,
+        horaOrcadas,
+        valorHora,
+        obs,
+        status,
+        prioridade,
+        variacao
+      }
+    })
+
+    revalidatePath('/seed.ts')
+  } catch (error) {
+     { error: 'Falhou a atualização dos dados' }
+  }
+}
+
+export async function deleteAlert(data:FormData): Promise<void> {
+  const id = data.get('id') as any
+
+  await prisma.alertItems.delete({
+    where: { id: 1 }
+  })
+
+  revalidatePath('/seed.ts')
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -12,12 +12,100 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { createAlert } from "@/app/components/actions/alertActions";
+import { createAlert, deleteAlert, updateAlert } from "@/app/components/actions/alertActions";
 
 export const HeaderAlert = () => {
     return (
         <div className="bg-amber-500 h-[10vh] items-center justify-items-center flex justify-between">
             <div className="text-center p-3 text-2xl">Capital Trade</div>
+
+            <div className="p-4">
+                <Dialog>
+
+                    <DialogTrigger asChild>
+                        <Button variant="outline">Update Item</Button>
+                    </DialogTrigger>
+
+                    <DialogContent>
+                        <form action={updateAlert}>
+                            <DialogHeader>
+                                <DialogTitle>Adicionar Item</DialogTitle>
+                            </DialogHeader>
+
+                            <div className="grid gap-3">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" name="name" />
+
+                                <Label htmlFor="responsavel">Responsavel</Label>
+                                <Input id="responsavel" name="responsavel" />
+
+                                <Label htmlFor="dataAprovacao">Data de Aprovação</Label>
+                                <Input id="dataAprovacao" name="dataAprovacao" />
+
+                                <Label htmlFor="tipoCobranca">Tipo de Cobrança</Label>
+                                <Input id="tipoCobranca" name="tipoCobranca" />
+
+                                <Label htmlFor="ptax">PTAX</Label>
+                                <Input id="ptax" name="ptax" />
+
+                                <Label htmlFor="orcado">Orçado</Label>
+                                <Input id="orcado" name="orcado" />
+
+                                <Label htmlFor="realizado">Realizado</Label>
+                                <Input id="realizado" name="realizado" />
+
+                                <Label htmlFor="variacao">Variação</Label>
+                                <Input id="variacao" name="variacao" />
+
+                                <Label htmlFor="horaOrcadas">Horas Orçadas</Label>
+                                <Input id="horaOrcadas" name="horaOrcadas" />
+
+                                <Label htmlFor="valorHora">Valor Hora</Label>
+                                <Input id="valorHora" name="valorHora" />
+
+                                <Label htmlFor="obs">OBS</Label>
+                                <Input id="obs" name="obs" />
+
+                                <Label htmlFor="status">Status</Label>
+                                <Input id="status" name="status" />
+
+                                <Label htmlFor="prioridade">Prioridade</Label>
+                                <Input id="prioridade" name="prioridade" />
+                            </div>
+
+                            <DialogFooter>
+                                <DialogClose asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </DialogClose>
+                                <Button type="submit">Salvar</Button>
+                            </DialogFooter>
+                        </form>
+                    </DialogContent>
+                </Dialog>
+            </div>
+
+            <div className="p-4">
+                <Dialog>
+
+                    <DialogTrigger asChild>
+                        <Button variant="outline">Delete Item</Button>
+                    </DialogTrigger>
+
+                    <DialogContent>
+                        <form action={deleteAlert}>
+                            <DialogHeader>
+                                <DialogTitle>Delete Item</DialogTitle>
+                            </DialogHeader>
+                            <DialogFooter>
+                                <DialogClose asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </DialogClose>
+                                <Button type="submit">Salvar</Button>
+                            </DialogFooter>
+                        </form>
+                    </DialogContent>
+                </Dialog>
+            </div>
 
             <div className="p-4">
                 <Dialog>
