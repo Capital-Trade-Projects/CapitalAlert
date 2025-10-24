512fa9d685cd25742dcc78e571220967e5bbe023
Marcio Henrique Arrais <marcio.santos@capitaltrade.srv.br>
2025-10-24 11:14:18 -0300

    feat: 'enviando arquivos via bucket R2'

---
+++ app/api/files/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { ListObjectsV2Command, paginateListObjectsV2 } from "@aws-sdk/client-s3";
+import { r2 } from "@/lib/r2Client";
+
+export async function GET() {
+    try {
+        const paginator = paginateListObjectsV2(
+      {
+        client: r2,
+        pageSize: 1,
+      },
+      {
+        Bucket: process.env.R2_BUCKET,
+      }
+    );
+        const objects = []
+        for await ( const page of paginator) {
+            console.log(page)
+            objects.push(page.Contents.map((o) => o.Key));
+        } 
+        objects.forEach((objectList, pageNum) => {console.log(
+
+        `Page ${pageNum + 1}\n------\n${objectList.map((o) => `• ${o}`).join("\n")}\n`,
+
+      );    
+    });  
+        // const response = await r2.send(command);
+
+        // const files = 
+        // response.Contents?.map((file) => ({
+        //     name: file.Key,
+        //     size: file.Size,
+        //     lastModified: file.LastModified,
+        //     url: `${process.env.R2_PUBLIC_URL}/${file.Key}`, 
+        // })) ??[];
+
+        return NextResponse.json(paginator);
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({ error: err.message }, { status: 500 })
+    }
+}
---
+++ app/api/upload/route.js
@@ -0,0 +1,59 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma"
+
+const r2 = new S3Client({
+    region: 'auto',
+    endpoint: process.env.R2_ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    },
+});
+
+export async function POST(req) {
+    try {
+    const formData = await req.formData();
+    const file = formData.get('file');
+    const alertId = formData.get("alertId");
+
+    console.log(file)
+    console.log(alertId)
+
+    if (!file) {
+        return NextResponse.json({ error: "Nenhum arquivo enviado" }, { status: 400 });
+    }
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const fileKey = `${Date.now()}-${file.name}`;
+
+    const command = new PutObjectCommand({
+        Bucket: process.env.R2_BUCKET,
+        Key: fileKey,
+        Body: buffer,
+        ContentType: file.type,
+    });
+
+    await r2.send(command);
+
+    const fileUrl = `${process.env.R2_PUBLIC_URL}/${fileKey}`;
+
+    const savedFile = await prisma.file.create({
+        data: {
+            filename: file.name,
+            url: fileUrl,
+            alertId: alertId ? Number(alertId) : null,
+        },
+    });
+
+    return NextResponse.json({
+        sucess: true,
+        file: savedFile,
+    });
+} catch (error) {
+    console.error(error);
+    return NextResponse.json(
+        { error: error.message || "Erro ao fazer upload"},
+        { status: 500 }
+    );
+}
+}
